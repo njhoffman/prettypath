c6b55a88edf89633a4d458a04147a2b0097d7108
Nick Hoffman <njhoffman1982@gmail.com>
2023-03-29 21:52:37 -0500

    fix directory paths

--- prettypath/init.lua
+++ prettypath/init.lua
@@ -1,30 +1,33 @@
-local devicons = require('prettypath.devicons')
-local opts = require('prettypath.opts')
-local utils = require('prettypath.utils')
+local devicons = require("prettypath.devicons")
+local opts = require("prettypath.opts")
+local utils = require("prettypath.utils")
 
 local overriden = require("prettypath.override")
 devicons.override(overriden.icons)
 
 local stats = {
   unassigned = {},
-  missing    = {},
-  total      = 0,
+  missing = {},
+  total = 0,
   time_start = os.time(),
-  config     = opts,
+  config = opts,
 }
 
 local pretty_path = function(path, user_opts, path_type)
+  path = string.gsub(path, "/$", "")
   local dir = path:match("(.*[/\\])")
-  local pathinfo = { dir = dir or '', name = path or '', path = path }
-  if path_type ~= nil then pathinfo.type = path_type end
+  local pathinfo = { dir = dir or "", name = path or "", path = path }
+  if path_type ~= nil then
+    pathinfo.type = path_type
+  end
 
   if dir ~= nil then
     dir = string.gsub(dir, "^./", "")
-    pathinfo.dir = string.gsub(dir, os.getenv('HOME') or '', '~')
+    pathinfo.dir = string.gsub(dir, os.getenv("HOME") or "", "~")
     pathinfo.name = path:match("^.+/(.+)$")
   end
 
-  if (opts.stat_file) then
+  if opts.stat_file then
     local filestat = utils.file_stat(pathinfo.path)
     if filestat == nil then
       table.insert(stats.missing, path)
@@ -40,7 +43,7 @@ local pretty_path = function(path, user_opts, path_type)
   end
 
   pathinfo.icon_data = devicons.get(pathinfo.name, pathinfo.type)
-  if pathinfo.icon_data.name == 'Default' then
+  if pathinfo.icon_data.name == "Default" then
     table.insert(stats.unassigned, path)
   end
   local parsed_str = utils.format_path(pathinfo, user_opts)
@@ -51,9 +54,9 @@ return {
   stats = stats,
   pretty_path = pretty_path,
   pretty_dir = function(path, user_opts)
-    pretty_path(path, user_opts, 'directory')
+    pretty_path(path, user_opts, "directory")
   end,
   pretty_file = function(path, user_opts)
-    pretty_path(path, user_opts, 'file')
+    pretty_path(path, user_opts, "file")
   end,
 }

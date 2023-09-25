cdf907e225725e9030fe5fe8230c476f5e07bfe1
Nick Hoffman <njhoffman1982@gmail.com>
2023-09-25 02:29:32 -0500

    add timing to stats

--- bin/prettypath.lua
+++ bin/prettypath.lua
@@ -2,7 +2,7 @@
 
 local prettypath = require('prettypath')
 local opts = require('prettypath.opts')
-
+local start_time = os.time()
 local stdin = io.stdin:lines()
 for line in stdin do
   -- allow pipe to be interrupted without error
@@ -13,14 +13,21 @@ end
 
 local stats = prettypath.stats or {}
 
-if opts.output_stats == true then
+local elapsed_time = os.difftime(os.time(), start_time)
+
+if tostring(opts.output_stats) == "true" then
   print('\n------\n')
   for _, path in pairs(stats.unassigned) do
     print(path)
   end
 
-  print('\n\n' .. 'Unassigned:\t' .. #stats.unassigned ..
-    '\n' .. 'Missing:\t' .. #stats.missing ..
-    '\n' .. 'Total:\t\t' .. stats.total .. '\n')
+  local avg = elapsed_time / stats.total * 1000000
 
+  print(
+    '\n\n' .. 'Missing:\t' .. #stats.missing ..
+    '\n' .. 'Unassigned:\t' .. #stats.unassigned ..
+    '\n' .. 'Total:\t\t' .. stats.total ..
+    '\n' .. 'Percentage:\t' .. Math.floor(100 - (#stats.unassigned / stats.total * 100)) .. '%' ..
+    '\n' .. 'Average:\t' .. (math.floor(avg * 100) / 100) .. 'μs' ..
+    '\n')
 end
--- install.sh
+++ install.sh
@@ -2,3 +2,7 @@
 
 luarocks make prettypath-dev-2.rockspec
 asdf reshim
+
+cat files.txt | ./bin/prettypath.lua --format='%path% %icon% %dir%%name%'
+fd --hidden --one-file-system --follow --no-ignore-vcs --color=never --full-path . | ./bin/prettypath.lua --output_stats=true
+fd --hidden --one-file-system --follow --no-ignore-vcs --color=never --full-path . "$HOME" | ./bin/prettypath.lua --output_stats=true
